6a44ea51daa1554e5b1da6091634e767686ccc85
arihh <arihh0615@gmail.com>
2013-06-27 12:15:47 +0900

    unsigned column primary key no require.

--- lib/activerecord-unsigned-column/railtie.rb
+++ lib/activerecord-unsigned-column/railtie.rb
@@ -3,7 +3,9 @@ module ActiveRecord
     class Railtie < Rails::Railtie
       initializer 'activerecord-unsigned-column' do
         ActiveSupport.on_load :active_record do
-          require 'activerecord-unsigned-column/primary_key'
+          if defined?(ActiveRecord::ConnectionAdapters::AbstractMysqlAdapter::Column)
+            require 'activerecord-unsigned-column/primary_key'
+          end
         end
       end
     end
